df68c22f71b22292e57d057f48fdc7af0cd18ae0
Arvin Doriani <117814822+98Arvin@users.noreply.github.com>
2023-08-05 11:25:20 +0200

    Update 2D Dynamic List.py
    Added menu to add or remove items.

--- 2D Dynamic List.py
+++ 2D Dynamic List.py
@@ -9,14 +9,20 @@ def prettyPrint():
   print()
 
 while True:
-  name = input("What is your name? ").capitalize()
-  age = input("What is your age? ")
-  pref = input("Mac or PC? ").capitalize()
-  row =  [name, age, pref]
-  funList.append(row)
-  exit = input("Do you want to exit? Y/N: ")
-  if exit.strip().lower()[0] == "y":
-    break
-  
+  menu = input("Do you want to add or remove? ADD/REMOVE: ")
+  if menu.strip().lower()[0] == "a":
+    name = input("What is your name? ").capitalize()
+    age = input("What is your age? ")
+    pref = input("Mac or PC? ").capitalize()
+    row =  [name, age, pref]
+    funList.append(row)
+    exit = input("Do you want to exit? Y/N: ")
+    if exit.strip().lower()[0] == "y":
+      break
+  else:
+    name = input("What do you want to delete? ")
+    for row in funList:
+      if name in row:
+        funList.remove(name)
 
 prettyPrint()
